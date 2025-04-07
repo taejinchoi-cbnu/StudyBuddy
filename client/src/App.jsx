4a3fcc1a840d4a46594a320f061206c5eea3be11
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-04-07 19:59:35 +0900

    Added docs folder and tested frontend-backend integration with API call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="container">
       <h1>StudyBuddy App</h1>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/group" element={<div>Group Page</div>} />
         <Route path="/schedule" element={<div>Schedule Page</div>} />
         <Route path="/chat" element={<div>Chat Page</div>} />
