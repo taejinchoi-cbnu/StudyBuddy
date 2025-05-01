7dbb2a3bdcbaa1618bc151ea10325f98838f97d3
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-05-01 22:35:57 +0900

    design: 폰트 변경, fix: firebase 수정

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -17,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const firestore = db; // firestore 별칭 추가
